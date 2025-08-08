0da5417d36912a81938148fd56789c8a0130e656
RjY <ry222@umsystem.edu>
2025-08-08 16:15:35 -0500

    Minor changes

--- src/nn/base.py
+++ src/nn/base.py
@@ -1,6 +1,37 @@
 import typing
+from typing import List
 import torch
 import torch.nn as nn
+
+
+__author__ = "Raghu Yelugam"
+__copyright__ = "Copyright 2025"
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Raghu Yelugam"
+__status__ = "Development"
+__date__ = "2025.06.28"
+__all__ = ["base_nn"]
+
+
+class AttentionHead(nn.Module):
+    """
+    A simple AttentionHead
+    """
+    def __init__(self,
+                  n_embed: int,
+                    n_head: int) -> None:
+        super().__init__()
+        self.Q = nn.Linear(n_embed, n_head)
+        self.K = nn.Linear(n_embed, n_head)
+        self.V = nn.Linear(n_embed, n_head)
+
+    def forward(self, 
+                h: torch.Tensor) -> List[torch.Tensor]
+
+
+
+
 
 class base_nn(nn.Module):
     """
@@ -12,7 +43,7 @@ class base_nn(nn.Module):
 
     @classmethod
     def encoder(cls):
-        pass
+        pass  
 
     @classmethod
     def decoder(cls):
