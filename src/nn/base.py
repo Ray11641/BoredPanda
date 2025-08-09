c857210993fcfedcf847f02a4d78b2996ea41027
Ray11641 <ry222@mst.edu>
2025-08-09 03:45:50 -0500

    Need to complete Encoder and Decoder stack

--- src/nn/base.py
+++ src/nn/base.py
@@ -1,5 +1,6 @@
 import typing
-from typing import List
+from typing import List, Dict
+import math
 import torch
 import torch.nn as nn
 
@@ -19,22 +20,56 @@ class AttentionHead(nn.Module):
     A simple AttentionHead
     """
     def __init__(self,
-                  n_embed: int,
-                    n_head: int) -> None:
+                  d_embed: int,
+                    d_head: int) -> None:
         super().__init__()
-        self.Q = nn.Linear(n_embed, n_head)
-        self.K = nn.Linear(n_embed, n_head)
-        self.V = nn.Linear(n_embed, n_head)
+        self.Q = nn.Linear(d_embed, d_head)
+        self.K = nn.Linear(d_embed, d_head)
+        self.V = nn.Linear(d_embed, d_head)
+
+    def scaled_dot_product_attention(Q: torch.Tensor,
+                                     K: torch.Tensor,
+                                     V: torch.Tensor
+                                     ) -> torch.Tensor:
+        """
+        Compute the scaled dot product attention for given query (Q), and
+          key-value (K-V) pairs
+        """
+        n_k = Q.shape(-1)
+            prod = torch.bmm(Q, K.transpose(1, 2)) / math.sqrt(n_k)
+        weights = nn.functional.softmax(prod, dim = -1)
+        return torch.bmm(weights, V)
 
     def forward(self, 
-                h: torch.Tensor) -> List[torch.Tensor]
+                h: torch.Tensor) -> List[torch.Tensor]:
+        attn_outs = self.scaled_dot_product_attention(
+                                                      Q = self.Q(h),
+                                                      K = self.K(h),
+                                                      V = self.V(h)
+                                                     )
+        return attn_outs
 
+class MultiHeadAttention(nn.Module):
+    """
+    Uses Attention Head for computation
+    """
+    def __init__(self,
+                 d_embed: int,
+                 d_head: int,
+                 n_heads: int) -> None:
+        super()._init__()
+        # raise error if d_embed != d_head*n_heads
+        assert d_embed == d_head*n_heads
+        self.heads = nn.ModuleList([AttentionHead(d_embed, d_head) for _ in range(n_heads)])
+        self.multihead_linear = nn.Linear(d_embed, d_embed)
 
-
-
+    def forward(self,
+                h: torch.Tensor) -> torch.Tensor:
+        multihead_out = torch.cat([head(h) for head in self.heads], dim = -1)
+        return self.multihead_linear(multihead_out)
 
 class base_nn(nn.Module):
-    """
+    """f
     create a base class for constructing custom transformer
     
     """
@@ -43,7 +78,7 @@ class base_nn(nn.Module):
 
     @classmethod
     def encoder(cls):
-        pass  
+        
 
     @classmethod
     def decoder(cls):
